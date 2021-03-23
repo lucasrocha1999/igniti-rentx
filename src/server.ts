dc2b31f919a808ca62b49612fa9aad6ff4fad506
Lucas Lira <lucasrocha446@gmail.com>
2021-03-23 20:10:15 -0300

    feat: add multer for upload

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import { router } from './routes';
 
 const app = express();
 
-app.use(router);
-
 app.use(express.json());
+
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
