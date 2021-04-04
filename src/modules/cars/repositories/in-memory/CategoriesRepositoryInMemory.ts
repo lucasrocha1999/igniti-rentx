5b669c8445f9805282d49eca403065b826a78550
Lucas Lira <lucasrocha446@gmail.com>
2021-04-03 23:28:44 -0300

    feat: adding tests in application

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,36 @@
+import { Category } from '../../entities/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../ICategoriesRepository';
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find(category => category.name === name);
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    // return this.categories;
+    const all = this.categories;
+    return all;
+  }
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    // const category = {
+    //   name,
+    //   description,
+    //   created_at: new Date(),
+    // };
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
