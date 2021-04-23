f4df54a830365836cbdc062833a451fb1b8ae3f7
Lucas Lira <lucasrocha446@gmail.com>
2021-04-23 09:13:09 -0300

    feat: add Local Storage Provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,10 +4,17 @@ import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider';
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './StorageProvader/implementations/LocalStorageProvider';
+import { IStorageProvider } from './StorageProvader/IStorageProvader';
 
 container.registerSingleton<IDateProvider>('DateProvider', DayjsDateProvider);
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider()
+);
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  LocalStorageProvider
 );
